2cfd5cbd98cb1d888fd85b2250e0674bb41c5339
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-04-25 11:51:21 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/sistemarh/PessoaColaboradora.java
@@ -0,0 +1,26 @@
+package com.trybe.acc.java.sistemarh;
+
+public class PessoaColaboradora {
+  public double calcularSalarioLiquido() {
+    return 0;
+  }
+
+  public double calcularValorBrutoRecebidoAnual(int ano) {
+    return 0;
+  }
+
+  public double calcularValorLiquidoRecebidoAnual(int ano) {
+    return 0;
+  }
+
+  public double calcularValorImpostoAnual(int ano) {
+    return 0;
+  }
+
+  public double getSalarioBruto() {
+    return 0;
+  }
+
+  public void setSalarioBruto(double salarioBruto) {}
+
+}
