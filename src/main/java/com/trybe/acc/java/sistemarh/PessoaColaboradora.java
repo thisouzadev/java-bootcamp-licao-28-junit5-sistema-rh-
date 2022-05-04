a335a1c8d918fd10b014edcb712d8f9db0507551
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-05-04 12:33:05 -0300

    finish

--- src/main/java/com/trybe/acc/java/sistemarh/PessoaColaboradora.java
+++ src/main/java/com/trybe/acc/java/sistemarh/PessoaColaboradora.java
@@ -1,26 +1,49 @@
 package com.trybe.acc.java.sistemarh;
 
 public class PessoaColaboradora {
+  double salarioBruto = 2200;
+
+  /**
+   * Método que cálcula salário líquido.
+   * 
+   */
   public double calcularSalarioLiquido() {
-    return 0;
+    return this.salarioBruto * 0.8;
   }
 
+  /**
+   * Método que cálcula salário bruto anual.
+   */
   public double calcularValorBrutoRecebidoAnual(int ano) {
-    return 0;
+    int totalDeMeses = 12 * ano;
+    double salarioBrutoRecebidoAnos = totalDeMeses * this.salarioBruto;
+    return salarioBrutoRecebidoAnos;
   }
 
+  /**
+   * Método que cálcula valor líquido recebido anual.
+   */
   public double calcularValorLiquidoRecebidoAnual(int ano) {
-    return 0;
+    int totalDeMeses = 12 * ano;
+    double salarioLiquidoRecebidoAnos = totalDeMeses * this.calcularSalarioLiquido();
+    return salarioLiquidoRecebidoAnos;
   }
 
+  /**
+   * Método que cálcula valor imposto anual.
+   */
   public double calcularValorImpostoAnual(int ano) {
-    return 0;
+    double salarioBruto = this.calcularValorBrutoRecebidoAnual(ano);
+    double salarioLiquido = this.calcularValorLiquidoRecebidoAnual(ano);
+    return salarioBruto - salarioLiquido;
   }
 
   public double getSalarioBruto() {
-    return 0;
+    return salarioBruto;
   }
 
-  public void setSalarioBruto(double salarioBruto) {}
+  public void setSalarioBruto(double salarioBruto) {
+    this.salarioBruto = salarioBruto;
+  }
 
 }
--- src/test/java/com/trybe/acc/java/sistemarh/PessoaColaboradoraTest.java
+++ src/test/java/com/trybe/acc/java/sistemarh/PessoaColaboradoraTest.java
@@ -1,6 +1,6 @@
 package com.trybe.acc.java.sistemarh;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,28 +10,36 @@ public class PessoaColaboradoraTest {
   @Test
   @DisplayName("10. Descrição do teste 1 aqui")
   public void testeCalcularSalarioLiquido() {
-    fail("Não implementado");
+    PessoaColaboradora pessoaColaboradora = new PessoaColaboradora();
+    pessoaColaboradora.setSalarioBruto(3000);
+    assertEquals(2400, pessoaColaboradora.calcularSalarioLiquido());
   }
 
 
   @Test
   @DisplayName("11. Descrição do teste 2 aqui")
   public void testeCalcularValorBrutoRecebidoAnual() {
-    fail("Não implementado");
+    PessoaColaboradora pessoaColaboradora = new PessoaColaboradora();
+    pessoaColaboradora.setSalarioBruto(7430.00);
+    assertEquals(24000.00, pessoaColaboradora.calcularValorBrutoRecebidoAnual(2));
   }
 
 
   @Test
   @DisplayName("12. Descrição do teste 3 aqui")
   public void testeCalcularValorLiquidoRecebidoAnual() {
-    fail("Não implementado");
+    PessoaColaboradora pessoaColaboradora = new PessoaColaboradora();
+    pessoaColaboradora.setSalarioBruto(3000);
+    assertEquals(180000, pessoaColaboradora.calcularValorBrutoRecebidoAnual(5));
   }
 
 
   @Test
   @DisplayName("13. Descrição do teste 4 aqui")
   public void testeCalcularValorImpostoAnual() {
-    fail("Não implementado");
+    PessoaColaboradora pessoaColaboradora = new PessoaColaboradora();
+    pessoaColaboradora.setSalarioBruto(7430);
+    assertEquals(445800, pessoaColaboradora.calcularValorBrutoRecebidoAnual(5));
   }
 
 }
